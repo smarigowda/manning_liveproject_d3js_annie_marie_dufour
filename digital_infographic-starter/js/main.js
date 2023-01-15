78502bbd24110b9b134812dc4f202710703f8480
Santosh Marigowda <santosharakere@gmail.com>
2023-01-15 06:08:01 +0000

    step 4

--- digital_infographic-starter/js/main.js
+++ digital_infographic-starter/js/main.js
@@ -1,1 +1,29 @@
-d3.select("body").style("background-color", "pink");
+const topRockAlbums = [
+  { artist: "Queen", title: "Greatest Hits", eq_albums: 929000 },
+  { artist: "Elton John", title: "Diamonds", eq_albums: 743000 },
+  { artist: "Fleetwood Mac", title: "Rumours", eq_albums: 721000 },
+  { artist: "CCR", title: "The 20 Greatest Hits", eq_albums: 630000 },
+  { artist: "Journey", title: "Journey's Greatest Hits", eq_albums: 561000 },
+];
+
+const topAlbumsSection = d3.select("#top-albums");
+
+topAlbumsSection.append("h3").text("Top Rock Albums");
+
+const barChartWidth = 500;
+const barChartHeight = 130;
+const barChart = topAlbumsSection
+  .append("svg")
+  .attr("viewbox", [0, 0, barChartWidth, barChartHeight])
+  .attr("width", barChartWidth)
+  .attr("height", barChartHeight);
+
+const marginLeft = 200;
+barChart
+  .append("line")
+  .attr("x1", marginLeft)
+  .attr("y1", 0)
+  .attr("x2", marginLeft)
+  .attr("y2", barChartHeight)
+  .attr('stroke', "#333")
+  .attr("stroke-width", 2)
